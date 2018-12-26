829dea209c8c59e92412e384232392881bc9d121
Gaetan35 <39278545+Gaetan35@users.noreply.github.com>
2018-12-26 18:31:28 +0100

    Add files via upload
    Nettoyage du code de modèle poisson. Prise en compte de la saison 2014-2015 pour le benchmark

--- Code/benchmark.py
+++ Code/benchmark.py
@@ -3,8 +3,6 @@ import numpy as np
 
 from modele_poisson import *
 from modele_random import *
-#from modele_classement import *
-
 
 tab2013 = pd.read_csv('2013_2014.csv')
 tab2014 = pd.read_csv('2014_2015.csv')
@@ -17,7 +15,7 @@ def test_prediction(tab_training, tab_testing, fonction_prediction, training_siz
     
     total_match_count = tab_testing['FTHG'].shape[0]
     S = 0
-    l=[]
+    
     for i in range(training_size, training_size+total_match_count):
         # On enregistre le résultat de chaque match 
         if tab_testing['FTHG'][i]>tab_testing['FTAG'][i]:
@@ -27,11 +25,11 @@ def test_prediction(tab_training, tab_testing, fonction_prediction, training_siz
         else:
             res = 'Egalité'
         # On prévoit le résultat du match 
+        #print('HomeTeam : {}, AwayTeam : {}, Index i = {}'.format(tab_testing['HomeTeam'][i], tab_testing['AwayTeam'][i], i))
         p = fonction_prediction(tab_training,tab_testing['HomeTeam'][i],tab_testing['AwayTeam'][i])
-        l.append([p,res])
         
         if p == res : 
-            S+=1
+            S += 1
     
     resultat_prediction = S/(total_match_count) * 100
     
@@ -40,27 +38,26 @@ def test_prediction(tab_training, tab_testing, fonction_prediction, training_siz
 
 def plot_result(training_size=190):
 
-    x = ['2013-2014', '2015-2016', '2016-2017', '2017-2018']
+    x = ['2013-2014', '2014-2015', '2015-2016', '2016-2017', '2017-2018']
     
     random2013 = test_prediction(tab2013[:training_size], tab2013[training_size:], prediction_random, training_size)
-    #random2014 = test_prediction(tab2014, prediction_random)
+    random2014 = test_prediction(tab2014[:training_size], tab2014[training_size:], prediction_random, training_size)
     random2015 = test_prediction(tab2015[:training_size], tab2015[training_size:], prediction_random, training_size)
     random2016 = test_prediction(tab2016[:training_size], tab2016[training_size:], prediction_random, training_size)
     random2017 = test_prediction(tab2017[:training_size], tab2017[training_size:], prediction_random, training_size)
     
-    y_random = [random2013, random2015, random2016, random2017]
+    y_random = [random2013, random2014, random2015, random2016, random2017]
     plt.plot(x, y_random, '-o', label='Modèle Aléatoire')
     
     poisson2013 = test_prediction(tab2013[:training_size], tab2013[training_size:], proba_gagnant, training_size)
-    #poisson2014 = test_prediction(tab2014, proba_gagnant)
+    poisson2014 = test_prediction(tab2014[:training_size], tab2014[training_size:], proba_gagnant, training_size)
     poisson2015 = test_prediction(tab2015[:training_size], tab2015[training_size:], proba_gagnant, training_size)
     poisson2016 = test_prediction(tab2016[:training_size], tab2016[training_size:], proba_gagnant, training_size)
     poisson2017 = test_prediction(tab2017[:training_size], tab2017[training_size:], proba_gagnant, training_size)
     
-    y_poisson = [poisson2013,  poisson2015, poisson2016, poisson2017]
+    y_poisson = [poisson2013, poisson2014, poisson2015, poisson2016, poisson2017]
     print(y_poisson)
     
-
     plt.plot(x, y_poisson, '-o', label='Modèle Poisson')
     
     plt.xlabel('Saisons')
--- Code/modele_poisson.py
+++ Code/modele_poisson.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  3 17:51:33 2018
-@author: louisladuve
-"""
-
 import pandas as pd
 from math import *
 import numpy as np
@@ -12,112 +5,77 @@ import random
 from datetime import *
 from datetime import datetime
 
-tab = pd.read_excel('SaisonPL2017.xlsx')
-tab2018 = pd.read_excel('SaisonPL2018_2019_encours 2.xlsx')
-calendrier_PL2018 = pd.read_excel('Classeur3.xlsx')
 
-
-
-""" 
-Correspondance des tableaux Excel pour l'ecriture des équipes  
-  
-"""
-## code V1.2
-
-equipesres = ['Man City','Tottenham','Man United','Leicester','Wolves','Brighton','West Ham','Newcastle','Huddersfield','Cardiff']
-equipescal = ['Manchester City','Tottenham Hotspur','Manchester United','Leicester City','Wolverhampton Wanderers','Brighton and Hove Albion','West Ham United','Newcastle United','Huddersfield Town','Cardiff City']                     
-
-def changement_nom_equipes(tab,equipescal,equiperes):
-    
-    
-    for i in range(380):
-        for j in range(len(equipesres)):
-            if calendrier_PL2018['HOME TEAM'][i]==equipescal[j]:
-                calendrier_PL2018['HOME TEAM'][i]=equipesres[j]
-            
-            if calendrier_PL2018['AWAY TEAM'][i]==equipescal[j]:
-                calendrier_PL2018['AWAY TEAM'][i]=equipesres[j]
-                
-    return 'Equipes Changées'
-
-
-### changemenet des noms des équipes du calendrier calendrier_PL2018 pour qu'elles correspondent avec celles de tab2018 les résultats de la saison en cours.
-#changement_nom_equipes(tab,equipescal,equipesres) 
-
-
-""" Calcul nombre de but total de l'equipe home marqués à domicile durant la saison """
 def calcul_nb_but_dom(tab , equipe):
+    """ Calcul nombre de but total de l'equipe home marqués à domicile durant la saison """
     home_goals = tab.loc[tab['HomeTeam']== equipe]['FTHG'].sum()
     return home_goals
 
-""" Calcul nombre de but total de l'equipe 'équipe' marqués à l'extérieur durant la saison """
+
 def calcul_nb_but_ext(tab,equipe):
+    """ Calcul nombre de but total de l'equipe 'équipe' marqués à l'extérieur durant la saison """
     away_goals = tab.loc[tab['AwayTeam']==equipe]['FTAG'].sum()
     return away_goals
 
-""" Le potentiel d'attaque de l'équipe 'equipe' à domicile """
+
 def potentiel_attaque_dom(tab,equipe):
+    """ Le potentiel d'attaque de l'équipe 'equipe' à domicile """
     home_match_count = tab.loc[tab['HomeTeam']==equipe].shape[0]
+    
+    #print('Nb buts marqué à domicile : {}, home_match_count : {}, Equipe : {}'.format(calcul_nb_but_dom(tab,equipe), home_match_count, equipe))
     return calcul_nb_but_dom(tab,equipe)/home_match_count
 
-""" Le potentiel d'attaque de l'équipe 'equipe' à l'extérieur """
+
 def potentiel_attaque_ext(tab,equipe):
+    """ Le potentiel d'attaque de l'équipe 'equipe' à l'extérieur """
     away_match_count = tab.loc[tab['AwayTeam']==equipe].shape[0]
     return calcul_nb_but_ext(tab,equipe)/away_match_count
 
-""" calcul du nombre de but pris à domicile par l'équipe 'equipe' """
+
 def calcul_nb_but_pris_dom(tab,equipe):
+    """ calcul du nombre de but pris à domicile par l'équipe 'equipe' """
     home_goals_taken = tab.loc[tab['HomeTeam']== equipe]['FTAG'].sum()
     return home_goals_taken
 
-""" calcul du nombre de but pris à l'extérieur par l'équipe 'equipe' """
+
 def calcul_nb_but_pris_ext(tab,equipe):
+    """ calcul du nombre de but pris à l'extérieur par l'équipe 'equipe' """
     away_goals_taken = tab.loc[tab['AwayTeam']== equipe]['FTHG'].sum()
     return away_goals_taken
 
-""" Le potentiel de défense de l'équipe 'equipe' à domicile """
+
 def potentiel_defense_dom(tab,equipe):
+    """ Le potentiel de défense de l'équipe 'equipe' à domicile """
     home_match_count = tab.loc[tab['HomeTeam']==equipe].shape[0]
     return calcul_nb_but_pris_dom(tab,equipe)/home_match_count
 
-""" Le potentiel de défense de l'équipe 'equipe' à l'extérieur """
+
 def potentiel_defense_ext(tab,equipe):
+    """ Le potentiel de défense de l'équipe 'equipe' à l'extérieur """
     away_match_count = tab.loc[tab['AwayTeam']==equipe].shape[0]
     return calcul_nb_but_pris_ext(tab,equipe)/away_match_count
 
-""" créer la liste des équipes  """
-def get_teams_list(tab):
-    return tab['HomeTeam'].drop_duplicates()
 
-""" écrit dans un nouvel excel """
-def write_data_excel(tab):
-    writer = pd.ExcelWriter('output.xlsx')
-    new_tab = get_teams_list(tab)
-    new_tab.to_excel(writer, index=False)
-
-#write_data_excel(tab)
-
-""" Calcul nombre de but total marqués à domicile durant la saison """
 def nombre_moyen_but_ligue_dom(tab):
+    """ Calcul nombre de but total marqués à domicile durant la saison """
     total_home_goals = tab['FTHG'].sum() 
     total_match_count = tab['FTHG'].shape[0]
     
     return total_home_goals / total_match_count
 
-""" Calcul nombre de but total marqués à l'extérieur durant la saison """
+
 def nombre_moyen_but_ligue_ext(tab):
+    """ Calcul nombre de but total marqués à l'extérieur durant la saison """
     total_away_goals = tab['FTAG'].sum() 
     total_match_count = tab['FTAG'].shape[0]
     
     return total_away_goals / total_match_count
 
-
-""" 
-calcul des forces d'attaque et de défense pour l'équipe 'equipe' 
-à domicile ou exterieur 
-"""
-
+# calcul des forces d'attaque et de défense pour l'équipe 'equipe' 
+# à domicile ou exterieur 
+    
 def force_attaque_dom(tab,equipe):
+    #print('Potentiel attaque : {}'.format(potentiel_attaque_dom(tab,equipe)))
     return  potentiel_attaque_dom(tab,equipe) / nombre_moyen_but_ligue_dom(tab)
     
 def force_attaque_ext(tab,equipe):
@@ -130,36 +88,37 @@ def force_defense_ext(tab,equipe):
     return  potentiel_defense_ext(tab,equipe) / nombre_moyen_but_ligue_dom(tab)
 
 
-
-
-""" --------- Parametre lambda de la loi de poisson ----------  """
+# --------- Parametre lambda de la loi de poisson ----------  
 
 def lambda_dom(tab,equipe_dom,equipe_ext):
+    #print("Force attaque : {}, force defense : {}, nombre_moyen_buts : {}".format(force_attaque_dom(tab,equipe_dom), force_defense_ext(tab,equipe_ext), nombre_moyen_but_ligue_dom(tab)))
     return force_attaque_dom(tab,equipe_dom) * force_defense_ext(tab,equipe_ext) * nombre_moyen_but_ligue_dom(tab)
 
 def lambda_ext(tab,equipe_dom,equipe_ext):
     return force_attaque_ext(tab,equipe_ext) * force_defense_dom(tab,equipe_dom) * nombre_moyen_but_ligue_ext(tab)
 
-def lambda_dom_pondere(tab,equipe_dom,equipe_ext, ponderation=0):
+def lambda_dom_pondere(tab,equipe_dom,equipe_ext, ponderation=0.1):
+    """ Pondère la valeur du lambda suivant le résultat du dernier match joué 
+    entre les 2 équipes """
     
     match_precedent = tab.loc[(tab['HomeTeam']== equipe_ext) & (tab['AwayTeam']==equipe_dom)]
-
-    
     lam_equipe_dom = lambda_dom(tab, equipe_dom, equipe_ext)
+    
     if not match_precedent.empty:
         if (match_precedent['FTHG'] > match_precedent['FTAG']).bool():
             lam_equipe_dom *= 1 - ponderation
         elif (match_precedent['FTHG'] < match_precedent['FTAG']).bool():
             lam_equipe_dom *= 1 + ponderation
 
-
     return lam_equipe_dom
 
-def lambda_ext_pondere(tab,equipe_dom,equipe_ext, ponderation=0):
-    match_precedent = tab.loc[(tab['HomeTeam']== equipe_ext) & (tab['AwayTeam']==equipe_dom)]
-
+def lambda_ext_pondere(tab,equipe_dom,equipe_ext, ponderation=0.1):
+    """ Pondère la valeur du lambda suivant le résultat du dernier match joué 
+    entre les 2 équipes """
     
+    match_precedent = tab.loc[(tab['HomeTeam']== equipe_ext) & (tab['AwayTeam']==equipe_dom)]
     lam_equipe_ext = lambda_ext(tab, equipe_dom, equipe_ext)
+    
     if not match_precedent.empty:
         if (match_precedent['FTHG'] > match_precedent['FTAG']).bool():
             lam_equipe_ext *= 1 + ponderation
@@ -167,32 +126,37 @@ def lambda_ext_pondere(tab,equipe_dom,equipe_ext, ponderation=0):
         elif (match_precedent['FTHG'] < match_precedent['FTAG']).bool():
             lam_equipe_ext *= 1 - ponderation
 
-
-
     return lam_equipe_ext
     
     
-
-
-""" 
-Pour un match donné entre equipe_dom et equipe_ext, retourne le résultat du match; 
-et les probabilité de chaque possibilité (1,N,2)
-"""
 def proba_gagnant(tab,equipe_dom,equipe_ext):
+    """ Pour un match donné entre equipe_dom et equipe_ext, retourne le 
+    résultat du match et les probabilité de chaque possibilité (1,N,2)"""
+
     L1=[]
     L2=[]
-    lambda1 = lambda_dom_pondere(tab,equipe_dom,equipe_ext, 0)
-    lambda2 = lambda_ext_pondere(tab,equipe_dom,equipe_ext, 0)
+    
+    # Calcul des lambdas
+    lambda1 = lambda_dom_pondere(tab,equipe_dom,equipe_ext, 0.1)
+    lambda2 = lambda_ext_pondere(tab,equipe_dom,equipe_ext, 0.1)
+    
+    #print('Lambda1 : {}, Lambda2 : {}'.format(lambda1, lambda2))
+    
     for k in range(10):
-        #loi de Poisson 
+        # Loi de Poisson 
         L1.append( exp(-lambda1) * ((lambda1)**k) / factorial(k) )
         L2.append( exp(-lambda2) * ((lambda2)**k) / factorial(k) ) 
+        
     prob_dom_gagne=0
     prob_ext_gagne=0
+    
+    #print('L1 : {}, L2 : {} \n'.format(L1, L2))
+        
     for k in range(1,10):
         for l in range(k-1):
             prob_dom_gagne += L1[k] * L2[l]
             prob_ext_gagne += L1[l] * L2[k]
+            
     prob_egalite = 1-(prob_ext_gagne+prob_dom_gagne)
     
     # Afficage des probas simplifié : 
@@ -200,11 +164,7 @@ def proba_gagnant(tab,equipe_dom,equipe_ext):
     pe = int( 1000 * prob_egalite ) /10
     p2 = int( 1000 * prob_ext_gagne ) /10
     
-
-#    p1 =  prob_dom_gagne 
-#    pe =  prob_egalite
-#    p2 =  prob_ext_gagne 
-#       
+       
     if prob_egalite > prob_dom_gagne and prob_egalite > prob_ext_gagne:
         # Cas d'egalite 
         return 'Egalité'
@@ -216,74 +176,7 @@ def proba_gagnant(tab,equipe_dom,equipe_ext):
         return equipe_dom
     
 
-teams = get_teams_list(tab)
-
-
-"""
-Evaluation du programme sur des matchs déjà déroulés
-Test la fonction proba_gagnant() pour chaque match de tab, et compare avec le résultat réel du match avec la prédiction
-Retourne en % l'évaluation du programme 
-"""
-
-    
 
 
 
-#print(calcul_nb_but_dom(tab , 'Man City'))
-#
-#print(lambda_dom(tab,'Bournemouth','Man City'))
-#print(lambda_ext(tab,'Bournemouth','Man City'))
-#
-#print(proba_gagnant(tab,'Arsenal','Liverpool'))
-#print(proba_gagnant(tab,'Arsenal','Man City'))
-#
-#print(Test_prediction(tab))
-#print(Test_prediction(tab2018))
-#
-#print(Test_prediction(tab2017and2018))
 
-#print(proba_gagnant(tab2018,'Chelsea','Man United'))
-
-"""
-Prévision des matchs du week_end
-"""
-
-
-def matchs_we(tab,calendrier,date):
-    
-    total_match_count = calendrier['HOME TEAM'].shape[0]
-    
-    l=[]
-    
-    for i in range(total_match_count):
-        if calendrier['DATE'][i] == date:
-            l.append([calendrier['HOME TEAM'][i],calendrier['AWAY TEAM'][i]])
-    return l
-
-#week_end = ['2018-10-20 00:00:00','2018-10-21 00:00:00','2018-10-22 00:00:00']
-#
-#
-#d1 = datetime(2018, 11, 10, 0, 0)
-#d2 = datetime(2018, 11, 11, 0, 0)
-#d3 = datetime(2018, 11, 22, 0, 0)
-#
-#d = [d1,d2]
-#
-#l=[]
-#for j in range(len(d)):
-#    l += matchs_we(tab2018,calendrier_PL2018,d[j])
-##print(l)
-#
-#matchs_we1 = []
-#for j in range(len(l)):
-#    if l[j][0]== 'Wolverhampton Wanderers' or l[j][1]== 'Wolverhampton Wanderers' or l[j][0]=='Fulham' or l[j][1]== 'Fulham':                       
-#        pass
-#    else:  
-#        matchs_we1.append(proba_gagnant(tab2018,l[j][0],l[j][1]))
-
-#print(matchs_we1)
-
-#print(calendrier_PL2018['DATE'])
-
-
-#total_match_count = calendrier_PL2018['HOME TEAM'].shape[0]
