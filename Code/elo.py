7ace53607448238ddf82cd9f8e3ab833c67ec900
Gaetan35 <39278545+Gaetan35@users.noreply.github.com>
2019-02-06 15:24:22 +0100

    Add files via upload

--- Code/elo.py
+++ Code/elo.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from math import ceil, floor
 
+""" Implement an elo model based on this article : 
+    - https://stuartlacy.co.uk/2017/08/31/implementing-an-elo-rating-system-for-european-football/
+    
+    Uses this article to convert elo difference into winning probability :
+    - https://betting.betfair.com/football/premier-league/barclays-premier-league-coverting-ratings-to-odds-030515-186.html
+"""
+
 def expected_outcome(dr):
     """ dr is the elo difference between both teams
     dr = elo_home - elo_away """
