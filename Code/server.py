3d553182a90339c756a53319d3da7bd4bb012362
Gaetan35 <39278545+Gaetan35@users.noreply.github.com>
2019-02-06 15:17:18 +0100

    Add files via upload

---
+++ Code/server.py
@@ -0,0 +1,25 @@
+from flask import *
+from modele_poisson import *
+import pandas as pd
+
+app = Flask(__name__)
+
+FLASK_DEBUG=1
+
+@app.route('/', methods=['GET', 'POST'])
+def accueil():
+
+    #tab2017 = pd.read_csv(url_for('static', filename='2017_2018.csv'))
+    tab2017 = pd.read_csv('2017_2018.csv')
+    team_list = list(tab2017['HomeTeam'].drop_duplicates())
+    
+    if request.method == 'POST':
+        team1 = request.form['team1']
+        team2 = request.form['team2']
+        prediction = proba_gagnant(tab2017,team1, team2)
+        return render_template('index.html', team_list = team_list, prediction="Résultat : {}".format(prediction))
+    else:
+        return render_template('index.html', team_list = team_list, prediction="")
+
+if __name__ == '__main__':
+    app.run(debug=True)
